458882b58c4176a9691d67d11fc685df406037a5
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-07-27 11:37:23 +0300

    9.7 done

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -20,7 +20,7 @@
 
 
 
-const calculateExercises = (dailyHours: number[], dailyTarget: number) => {
+export const calculateExercises = (dailyHours: number[], dailyTarget: number) => {
 
     // number of days
     const days = dailyHours.length;
@@ -29,18 +29,16 @@ const calculateExercises = (dailyHours: number[], dailyTarget: number) => {
 
     const trainingDaysAndHours = dailyHours.filter(d => d > 0);
     const trainingDays = trainingDaysAndHours.length;
-    
-    // original target value
-    const targetValue = dailyTarget;
+
 
     // average time per day
     //sum
     let sumOfHours = 0;
     let count = 0;
     while (count < dailyHours.length) {
-        sumOfHours += dailyHours[count]
-        count++
-    };
+        sumOfHours += dailyHours[count];
+        count += 1;
+    }
     // sum divided with length
     const averageHoursPerDay = sumOfHours / dailyHours.length;
 
@@ -49,41 +47,54 @@ const calculateExercises = (dailyHours: number[], dailyTarget: number) => {
     // rating 1-3 how good target was met
     // 90% of target is 3, 70% of target is 2, 40% of target is 1, <40% is 0
     const performance = averageHoursPerDay / dailyTarget;
-    let rating = 0;
-    if ( performance >= 0.9 ) {
-        console.log('rating 3', performance)
-        let rating = 3
-    }
-    else if ( performance < 0.9 && performance >= 0.7) {
-        let rating = 2
-    }
-    else if ( performance < 0.7 && performance >= 0.4) {
-        let rating = 1    }
-    else if ( performance < 0.4) {
-        let rating = 0    };
 
     // text value explaining the rating
     const ratingExplanation = '90% of target is 3, 70% of target is 2, 40% of target is 1, <40% is 0';
 
-    return ` Number of days ${days}\n Number of training days ${trainingDays}\n Training target value per day: ${dailyTarget}\n Calculated average time ${averageHoursPerDay}\n Was target reached: ${isTargetMet}\n Rating for performance ${rating}\n Explanation for ratingsystem ${ratingExplanation}`;
+
+    if ( performance >= 0.9 ) {
+        console.log('rating 3', performance);
+        const rating = 3;
+        return ` Number of days ${days}\n Number of training days ${trainingDays}\n Training target value per day: ${dailyTarget}\n Calculated average time ${averageHoursPerDay}\n Was target reached: ${isTargetMet}\n Rating for performance ${rating}\n Explanation for ratingsystem ${ratingExplanation}`;
+
+    }
+    else if ( performance < 0.9 && performance >= 0.7) {
+        const rating = 2;
+        return ` Number of days ${days}\n Number of training days ${trainingDays}\n Training target value per day: ${dailyTarget}\n Calculated average time ${averageHoursPerDay}\n Was target reached: ${isTargetMet}\n Rating for performance ${rating}\n Explanation for ratingsystem ${ratingExplanation}`;
+
+    }
+    else if ( performance < 0.7 && performance >= 0.4) {
+        const rating = 1;    
+        return ` Number of days ${days}\n Number of training days ${trainingDays}\n Training target value per day: ${dailyTarget}\n Calculated average time ${averageHoursPerDay}\n Was target reached: ${isTargetMet}\n Rating for performance ${rating}\n Explanation for ratingsystem ${ratingExplanation}`;
+
+    }
+    else if ( performance < 0.4) {
+        const rating = 0;
+        return ` Number of days ${days}\n Number of training days ${trainingDays}\n Training target value per day: ${dailyTarget}\n Calculated average time ${averageHoursPerDay}\n Was target reached: ${isTargetMet}\n Rating for performance ${rating}\n Explanation for ratingsystem ${ratingExplanation}`;
+
+    }
+    else return null;
+
     
 
-}
+    
+
+};
 
 
-console.log(calculateExercises( [1,0,2], 2 ));
+//console.log(calculateExercises( [1,0,2], 2 ));
 
 
 
 
 
 
-/*
-const a: number[] = process.argv.slice(2).map(Number);
-const b: number = Number(process.argv[3]);
-console.log('function called:', calculateExercises(a, b));
 
-*/
+//const a: number[] = process.argv.slice(2).map(Number);
+//const b: number = Number(process.argv[3]);
+//console.log('function called:', calculateExercises(a, b));
+
+
 
 
 
--- index.ts
+++ index.ts
@@ -17,10 +17,9 @@
 
 
 
-
-import { calculateBmi } from './bmiCalculator';
-
-
+//importing files
+import { calculateBmi } from './bmiCalculator'
+import { calculateExercises } from './exerciseCalculator'
 import express = require('express');
 const app = express();
 
@@ -29,14 +28,35 @@ app.get('/bmi/:height/:weight', (req, res) => {
     "height": parseInt(req.params.height),
     "weight": parseInt(req.params.weight)
   }
-  
-  const data = calculateBmi(object.height, object.weight)
+
+  const data = calculateBmi(object.height, object.weight);
   res.send(data);
 });
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello full stack!')
-})
+  res.send('Hello full stack!');
+});
+
+
+app.post('/calculateExercise/:dailyHours/:target', (req, res) => {
+    console.log('inside POST')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment 
+  
+
+    console.log('params', req.params)
+    // splitting dailyHours in a array
+    const array = req.params.dailyHours.split(',')
+    // converting array of string to number
+    const numberArray = array.map(Number)
+  
+    // target to variable
+    const target = parseInt(req.params.target)
+
+    const result = calculateExercises(numberArray, target);
+    res.send(result);
+  });
+
+
 
 const PORT = 3003;
 
