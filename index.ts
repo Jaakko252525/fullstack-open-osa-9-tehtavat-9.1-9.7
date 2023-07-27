11de36cfbe4d7b786ae0e42fca8be8cb37f6c37a
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-07-27 11:46:36 +0300

    semicolons

--- index.ts
+++ index.ts
@@ -18,8 +18,8 @@
 
 
 //importing files
-import { calculateBmi } from './bmiCalculator'
-import { calculateExercises } from './exerciseCalculator'
+import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 import express = require('express');
 const app = express();
 
@@ -27,7 +27,7 @@ app.get('/bmi/:height/:weight', (req, res) => {
   const object = {
     "height": parseInt(req.params.height),
     "weight": parseInt(req.params.weight)
-  }
+  };
 
   const data = calculateBmi(object.height, object.weight);
   res.send(data);
@@ -39,18 +39,18 @@ app.get('/hello', (_req, res) => {
 
 
 app.post('/calculateExercise/:dailyHours/:target', (req, res) => {
-    console.log('inside POST')
+    console.log('inside POST');
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment 
   
 
-    console.log('params', req.params)
+    console.log('params', req.params);
     // splitting dailyHours in a array
-    const array = req.params.dailyHours.split(',')
+    const array = req.params.dailyHours.split(',');
     // converting array of string to number
-    const numberArray = array.map(Number)
+    const numberArray = array.map(Number);
   
     // target to variable
-    const target = parseInt(req.params.target)
+    const target = parseInt(req.params.target);
 
     const result = calculateExercises(numberArray, target);
     res.send(result);
